769f4119764697fe88395aacd671838509b8428f
robynnlouisee <robynnlouisee123@gmail.com>
2025-04-28 12:44:29 +0700

    Made it so when the user inputs a number a new grid is generated

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector('.container');
+const resetBtn = document.getElementById('reset-button');
+
 
 for (let i = 0; i < 256; i++) {
 const square = document.createElement('div');
@@ -11,8 +13,41 @@ square.addEventListener("mouseover", () => {
 
 }
 
-const resetBtn = document.getElementById('reset-button');
-
 resetBtn.addEventListener("click", () => {
-  let gridSize = prompt("Please enter the number of squares for the new grid:");
+  resetGrid();
 });
+
+const resetGrid = () => {
+    container.innerHTML = '';
+
+    let numSquares = prompt("Please enter the number of squares for the new grid:");
+    while (numSquares === '' || isNaN(numSquares) || numSquares <= 0 || numSquares > 100) {
+        if (numSquares === '') {
+            alert("Please enter a valid number.");
+        } else if (isNaN(numSquares)) {
+            alert("Please enter a valid number.");
+        } else if (numSquares > 100) {
+            alert("The maximum number of squares is 100.");
+        }
+
+        numSquares = prompt("Please enter the number of squares for the new grid:");
+    } 
+
+    numSquares = parseInt(numSquares);
+
+
+        const totalSquares = numSquares * numSquares;
+        for (let i = 0; i < totalSquares; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            container.appendChild(square); 
+     
+            square.addEventListener("mouseover", () => {
+                square.style.backgroundColor = "black";
+            });
+        }
+    };
+    
+    
+  
+
